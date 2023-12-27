692510f8734ae09ad6509fc77db9a54acfd8b3aa
Kyrill <kyrill@gobber.ch>
2023-12-27 16:37:43 +0100

    Added constants for teas, prev and next buttons

--- src/components/organisms/MainContent.tsx
+++ src/components/organisms/MainContent.tsx
@@ -1,18 +1,29 @@
 import { toast } from "sonner";
 import { Button } from "../ui/button";
 import { Card, CardHeader } from "../ui/card";
-import { Pause, Play, TimerReset } from "lucide-react";
+import {
+    ArrowBigLeft,
+    ArrowBigRight,
+    Pause,
+    Play,
+} from "lucide-react";
 import { useState } from "react";
+import { teas } from "@/lib/constants";
 
 export const MainContent = () => {
-    const [timerState, setTimerState] = useState<"running" | "stopped">("stopped");
+    const [timerState, setTimerState] = useState<"running" | "stopped">(
+        "stopped"
+    );
+    const defaultCountdown = teas[0].infusions[0].duration;
+    const [currentInfusion, setCurrentInfusion] = useState(1);
+    const [currentTime, setCurrentTime] = useState(defaultCountdown);
 
     const getIcon = () => {
         switch (timerState) {
             case "running":
-                return <Pause size={48} />;
+                return <Pause size={62} />;
             case "stopped":
-                return <Play size={48} />;
+                return <Play size={62} />;
         }
     };
 
@@ -20,10 +31,10 @@ export const MainContent = () => {
         <div className="flex flex-col justify-center items-center gap-8 mt-12">
             <Card>
                 <CardHeader>
-                    <h1>Timer will be here</h1>
+                    <h1>{currentTime}</h1>
                 </CardHeader>
             </Card>
-            <div className="flex flex-row">
+            <div className="flex flex-col">
                 <Button
                     className="p-8"
                     variant={"ghost"}
@@ -43,23 +54,32 @@ export const MainContent = () => {
                 >
                     {getIcon()}
                 </Button>
-                <Button
-                    className="p-8"
-                    variant={"ghost"}
-                    onClick={() => {
-                        toast("RESET??", {
-                            description: `${new Date().toLocaleDateString()} @ ${new Date().toLocaleTimeString()}`,
-                            action: {
-                                label: "X",
-                                onClick: () => {
-                                    console.log("clicked");
-                                },
-                            },
-                        });
-                    }}
-                >
-                    <TimerReset size={48} />
-                </Button>
+                <div className="flex flex-row gap-8">
+                    <Button
+                        className="p-8"
+                        variant={"ghost"}
+                        onClick={() => {
+                            setCurrentInfusion(currentInfusion - 1);
+                        }}
+                    >
+                        <span className="flex flex-col">
+                            <ArrowBigLeft size={48} />
+                            Previous
+                        </span>
+                    </Button>
+                    <Button
+                        className="p-8"
+                        variant={"ghost"}
+                        onClick={() => {
+                            setCurrentInfusion(currentInfusion + 1);
+                        }}
+                    >
+                        <span className="flex flex-col">
+                            <ArrowBigRight size={48} />
+                            Next
+                        </span>
+                    </Button>
+                </div>
             </div>
         </div>
     );
---
+++ src/lib/constants.ts
@@ -0,0 +1,84 @@
+export type Tea = {
+    name: string;
+    infusions: Infusion[];
+}
+
+export type Infusion = {
+    id: number;
+    duration: number;
+}
+
+export const teas: Tea[] = [
+    {
+        name: 'Green Tea',
+        infusions: [
+            {
+                id: 1,
+                duration: 60
+            },
+            {
+                id: 2,
+                duration: 25
+            },
+            {
+                id: 3,
+                duration: 35
+            }
+
+        ]
+    },
+    {
+        name: 'Pu-erh',
+        infusions: [
+            {
+                id: 1,
+                duration: 10
+            },
+            {
+                id: 2,
+                duration: 20
+            },
+            {
+                id: 3,
+                duration: 25
+            },
+            {
+                id: 4,
+                duration: 30
+
+            },
+            {
+                id: 5,
+                duration: 40
+            },
+            {
+                id: 6,
+                duration: 45
+            },
+            {
+                id: 7,
+                duration: 50
+            },
+            {
+                id: 8,
+                duration: 55
+            },
+            {
+                id: 9,
+                duration: 60
+            },
+            {
+                id: 10,
+                duration: 70
+            },
+            {
+                id: 11,
+                duration: 80
+            },
+            {
+                id: 12,
+                duration: 90
+            },
+        ]
+    },
+]
