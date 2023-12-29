dec1a1ce2a2266fb1b15ececb2ff5b513f127bb7
Kyrill <kyrill@gobber.ch>
2023-12-29 15:43:45 +0100

    feat: Can skip through tea infusions now

--- src/components/organisms/MainContent.tsx
+++ src/components/organisms/MainContent.tsx
@@ -1,34 +1,53 @@
 import { toast } from "sonner";
 import { Button } from "../ui/button";
 import { Card, CardHeader } from "../ui/card";
-import {
-    ArrowBigLeft,
-    ArrowBigRight,
-    Pause,
-    Play,
-} from "lucide-react";
-import { useState } from "react";
-import { teas } from "@/lib/constants";
+import { ArrowBigLeft, ArrowBigRight, Pause, Play } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Tea, teas } from "@/lib/constants";
+
+const getIcon = (timerState: TimerState) => {
+    switch (timerState) {
+        case "running":
+            return <Pause size={62} />;
+        case "stopped":
+            return <Play size={62} />;
+    }
+};
+
+type TimerState = "running" | "stopped";
 
 export const MainContent = () => {
-    const [timerState, setTimerState] = useState<"running" | "stopped">(
-        "stopped"
-    );
-    const defaultCountdown = teas[0].infusions[0].duration;
+    const [currentTea, setCurrentTea] = useState<Tea>(teas[0]);
+    const [timerState, setTimerState] = useState<TimerState>("stopped");
     const [currentInfusion, setCurrentInfusion] = useState(1);
-    const [currentTime, setCurrentTime] = useState(defaultCountdown);
+    const [currentTime, setCurrentTime] = useState(
+        teas[0].infusions[0].duration
+    );
 
-    const getIcon = () => {
-        switch (timerState) {
-            case "running":
-                return <Pause size={62} />;
-            case "stopped":
-                return <Play size={62} />;
-        }
+    const handleBrewButtonEvent = () => {
+        toast("BREW TIME", getAlertContent());
+        if (timerState === "running") setTimerState("stopped");
+        else setTimerState("running");
     };
+    const getAlertContent = () => {
+        return {
+            description: `${new Date().toLocaleDateString()} @ ${new Date().toLocaleTimeString()}`,
+            action: {
+                label: "X",
+                onClick: () => {
+                    console.log("clicked");
+                },
+            },
+        };
+    };
+
+    useEffect(() => {
+        setCurrentTime(teas[0].infusions[currentInfusion - 1].duration);
+    }, [currentInfusion]);
 
     return (
         <div className="flex flex-col justify-center items-center gap-8 mt-12">
+            <h1>{currentTea.name}</h1>
             <Card>
                 <CardHeader>
                     <h1>{currentTime}</h1>
@@ -38,26 +57,15 @@ export const MainContent = () => {
                 <Button
                     className="p-8"
                     variant={"ghost"}
-                    onClick={() => {
-                        toast("BREW TIME", {
-                            description: `${new Date().toLocaleDateString()} @ ${new Date().toLocaleTimeString()}`,
-                            action: {
-                                label: "X",
-                                onClick: () => {
-                                    console.log("clicked");
-                                },
-                            },
-                        });
-                        if (timerState === "running") setTimerState("stopped");
-                        else setTimerState("running");
-                    }}
+                    onClick={handleBrewButtonEvent}
                 >
-                    {getIcon()}
+                    {getIcon(timerState)}
                 </Button>
                 <div className="flex flex-row gap-8">
                     <Button
                         className="p-8"
                         variant={"ghost"}
+                        disabled={currentInfusion === 1}
                         onClick={() => {
                             setCurrentInfusion(currentInfusion - 1);
                         }}
@@ -70,6 +78,9 @@ export const MainContent = () => {
                     <Button
                         className="p-8"
                         variant={"ghost"}
+                        disabled={
+                            currentInfusion === currentTea.infusions.length
+                        }
                         onClick={() => {
                             setCurrentInfusion(currentInfusion + 1);
                         }}
