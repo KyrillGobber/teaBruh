6b3195771db428d0d879c61e6565fb90b017aa47
Kyrill <kyrill@gobber.ch>
2024-04-17 23:11:37 +0200

    chore: refactors and removed test tea

--- src/lib/hooks/useTimer.tsx
+++ src/lib/hooks/useTimer.tsx
@@ -1,18 +1,18 @@
-import { useEffect, useRef, useState } from "react";
-import useInterval from "./useInterval";
+import { useEffect, useRef, useState } from 'react';
+import useInterval from './useInterval';
 import audio from '@/assets/yay.mp3';
-import { toast } from "sonner";
-import { Tea } from "../constants";
+import { toast } from 'sonner';
+import { Tea } from '../constants';
 
 export type TimerState = 'running' | 'stopped';
 
 export default function useTimer(tea: Tea) {
+    const { startInterval, stopInterval } = useInterval();
     const [timerState, setTimerState] = useState<TimerState>('stopped');
     const [progress, setProgress] = useState(0);
     const [currentTime, setCurrentTime] = useState(tea.infusions[0].duration);
     const [currentInfusion, setCurrentInfusion] = useState(1);
     const [isLastInfusion, setIsLastInfusion] = useState(false);
-    const { startInterval, stopInterval } = useInterval();
     const fractionRef = useRef(100 / currentTime);
     const audioRef = useRef(new Audio(audio));
 
@@ -28,7 +28,7 @@ export default function useTimer(tea: Tea) {
             startInterval(() => {
                 setProgress((progress) => progress + fractionRef.current);
                 setCurrentTime((currentTime) => currentTime - 1);
-            }, 1000)
+            }, 1000);
         }
         if (timerState === 'stopped') {
             stopInterval();
@@ -49,11 +49,15 @@ export default function useTimer(tea: Tea) {
 
     useEffect(() => {
         setTimerState('stopped');
-        const resetInfusion = 1;
-        const newTeaTime = tea.infusions[resetInfusion - 1].duration;
+
+        //Set the first infusion of the new tea
+        const newTeaTime = tea.infusions[0].duration;
         setCurrentTime(newTeaTime);
-        setCurrentInfusion(resetInfusion);
+
+        //Reset stuff
+        setCurrentInfusion(1);
         setProgress(0);
+
         const newFraction = 100 / newTeaTime;
         fractionRef.current = newFraction;
     }, [tea]);
@@ -71,7 +75,6 @@ export default function useTimer(tea: Tea) {
     }, [currentInfusion]);
 
     const start = () => {
-        console.log('infusion in hook: ', currentInfusion);
         if (isLastInfusion) {
             setCurrentInfusion(1);
             setIsLastInfusion(false);
@@ -109,4 +112,3 @@ export default function useTimer(tea: Tea) {
         progress,
     };
 }
-
