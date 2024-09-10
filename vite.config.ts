b406fd0d13b071792321853594477d20313c52b6
Kyrill <kyrill@gobber.ch>
2024-09-10 17:34:21 +0200

    feat: pwa with vite-pwa, first attempt

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,42 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [
+        react(),
+        svgr(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true,
+            },
+            manifest: {
+                name: 'Teabruh',
+                short_name: 'Teabruh',
+                description: 'Brew tea, bruh!',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'public/coffee.svg',
+                        sizes: '192x192',
+                        type: 'image/svg+xml',
+                    },
+                    {
+                        src: 'public/coffee.svg',
+                        sizes: '512x512',
+                        type: 'image/svg+xml',
+                    },
+                ],
+            },
+        }),
+    ],
     resolve: {
         alias: {
-            "@": path.resolve(__dirname, "./src"),
+            '@': path.resolve(__dirname, './src'),
         },
     },
 });
